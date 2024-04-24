9c572727ed254c808623ac0d4ca512ba7d271df2
fjrodl <fjrodl@unileon>
2024-04-24 17:05:34 +0200

    Arranged forlders and added csv's and JASP files

---
+++ Latency/join.bash
@@ -0,0 +1,15 @@
+ 1479  cat audio_ext4_ftrace_* > parsed_audio_ext4_ftrace_complete_parsed.csv
+ 1481  cat audio_sealfs_ftrace_* > parsed_audio_sealfs_ftrace_complete_pased.csv
+ 1482  cat image_ext4_ftrace_* > parsed_image_ext4_ftrace_complete_parsed.csv
+ 1483  cat image_sealfs_ftrace_* > parsed_image_sealfs_ftrace_complete_parsed.csv
+ 1484  cat nav_ext4_ftrace_* > parsed_nav_ext4_ftrace_complete_parsed.csv
+ 1485  cat nav_sealfs_ftrace_* > parsed_nav_sealfs_ftrace_complete_parsed.csv
+
+
+ 1418  cat audio_* | wc -l
+ 1419  cat parse* | wc -l
+ 1425  cat image_* | wc -l
+ 1426  cat parsed_image_ext4_ftrace_complete_parsed.csv | wc -l
+ 1429  cat nav_* | wc -l
+ 1432  cat parsed_nav_ext4_ftrace_complete_parsed.csv | wc -l
+ 1433  cat nav_* | wc -l
